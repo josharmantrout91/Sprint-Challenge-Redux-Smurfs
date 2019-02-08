6f2cdcaf37df4f278f0bfaf0abbb76f64b5ef946
josharmantrout91 <josh.armantrout@gmail.com>
2019-02-08 11:40:24 -0500

    Reducers
    - added reducer and cases to reducers/index.js

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,66 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  FETCH_SMURFS_START,
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_FAILURE,
+  POST_SMURF_START,
+  POST_SMURF_SUCCESS,
+  POST_SMURF_FAILURE
+} from "../actions";
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  //  updatingSmurf: false,
+  //  deletingSmurf: false,
+  error: ""
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_SMURFS_START:
+      return {
+        ...state,
+        fetchingSmurfs: true,
+        error: ""
+      };
+    case FETCH_SMURFS_SUCCESS:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      };
+    case FETCH_SMURFS_FAILURE:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      };
+    case POST_SMURF_START:
+      return {
+        ...state,
+        addingSmurf: true,
+        error: ""
+      };
+    case POST_SMURF_SUCCESS:
+      return {
+        ...state,
+        addingSmurf: false,
+        smurfs: action.payload
+      };
+    case POST_SMURF_FAILURE:
+      return {
+        ...state,
+        addingSmurf: false,
+        error: action.payload
+      };
+
+    default:
+      return state;
+  }
+}
+
+export default reducer;
 
 /*
   You'll only need one smurf reducer for this project.
